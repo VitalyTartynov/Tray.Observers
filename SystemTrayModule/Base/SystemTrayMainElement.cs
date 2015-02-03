59dbba3cd3670096a2152c5e14a58320cef1ceb6
Виталий Тартынов <vitaliy.tartunov@rubius.com>
2015-02-03 13:26:16 +0600

    Добавил выгрузку

--- SystemTrayModule/Base/SystemTrayMainElement.cs
+++ SystemTrayModule/Base/SystemTrayMainElement.cs
@@ -8,8 +8,13 @@ namespace SystemTrayModule.Base
     {
         private readonly NotifyIcon _notifyIcon;
 
+        private readonly ContextMenu _contextMenu;
+
         public SystemTrayMainElement()
         {
+            _contextMenu = new ContextMenu();
+            _contextMenu.MenuItems.Add(new MenuItem("Exit", OnExitClick));
+
             _notifyIcon = new NotifyIcon
             {
                 Icon = Resources.system_network_icon,
@@ -19,11 +24,17 @@ namespace SystemTrayModule.Base
 
             _notifyIcon.MouseClick += OnMouseClick;
             _notifyIcon.MouseDoubleClick += OnMouseDoubleClick;
+            _notifyIcon.ContextMenu = _contextMenu;
+        }
+
+        private void OnExitClick(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
 
         private void OnMouseDoubleClick(object sender, MouseEventArgs mouseEventArgs)
         {
-            Application.Exit();
+            
         }
 
         private void OnMouseClick(object sender, MouseEventArgs mouseEventArgs)
