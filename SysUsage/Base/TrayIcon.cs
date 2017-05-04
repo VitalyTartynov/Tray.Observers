d3abc48e3348631ce4be3d23850e4d349a56399e
TartunovVA <Vitaliy.Tartunov@GMail.com>
2017-05-04 15:38:06 +0700

    Add cache for icons

--- SysUsage/Base/TrayIcon.cs
+++ SysUsage/Base/TrayIcon.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace SysUsageTrayMonitor
@@ -10,6 +12,7 @@ namespace SysUsageTrayMonitor
         private readonly Color _color;
         private readonly PerformanceCounter _counter;
         private readonly NotifyIcon _notifyIcon;
+        private readonly Dictionary<int, Icon> _iconCache = new Dictionary<int, Icon>();
 
         public TrayIcon(string name, Color color, PerformanceCounter counter, ContextMenu menu)
         {
@@ -25,13 +28,29 @@ namespace SysUsageTrayMonitor
 
         public void Update()
         {
-            if (_notifyIcon.Icon != null) IconHelper.Destroy(_notifyIcon.Icon.Handle);
-            _notifyIcon.Icon = IconHelper.Create(Convert.ToInt32(_counter.NextValue()), _color);
+            var value = Convert.ToInt32(_counter.NextValue());
+            if (_iconCache.ContainsKey(value))
+            {
+                _notifyIcon.Icon = _iconCache[value];
+                return;
+            }
+
+            var newIcon = IconHelper.Create(value, _color);
+            _iconCache.Add(value, newIcon);
+            _notifyIcon.Icon = newIcon;
         }
 
         public void Dispose()
         {
             _notifyIcon.Visible = false;
+
+            while (_iconCache.Any())
+            {
+                var currentPair = _iconCache.First();
+                IconHelper.Destroy(currentPair.Value.Handle);
+                _iconCache.Remove(currentPair.Key);
+            }
+
             _notifyIcon.Dispose();
         }
     }
